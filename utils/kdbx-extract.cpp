ecc11d3207fe5bacf3971de8976912b87a6a4a59
Felix Geyer <debfx@fobos.de>
2012-04-22 22:33:41 +0200

    kdbx-extract: Print error message when opening the database failed.

--- utils/kdbx-extract.cpp
+++ utils/kdbx-extract.cpp
@@ -66,7 +66,7 @@ int main(int argc, char **argv)
     reader.readDatabase(&dbFile, key);
 
     if (reader.hasError()) {
-        qCritical("Error while reading the database.");
+        qCritical("Error while reading the database:\n%s", qPrintable(reader.errorString()));
         return 1;
     }
 
