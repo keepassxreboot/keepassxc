79b6ff99e4397bfb568d72dcd4ec6f16cbbca17b
Felix Geyer <debfx@fobos.de>
2012-04-26 12:18:38 +0200

    Mark library include dirs as SYSTEM.
    Compilers will suppress warnings from files in those directories.

--- src/core/Endian.cpp
+++ src/core/Endian.cpp
@@ -17,14 +17,7 @@
 
 #include "Endian.h"
 
-#if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 3))
-#pragma GCC push_options
-#pragma GCC diagnostic ignored "-Wold-style-cast"
-#endif
 #include <QtCore/QtEndian>
-#if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 3))
-#pragma GCC pop_options
-#endif
 #include <QtCore/QIODevice>
 
 namespace Endian {
