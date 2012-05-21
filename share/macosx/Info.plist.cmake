fa79da37b6270c329fe289ef4dbd509d861a3ba0
Felix Geyer <debfx@fobos.de>
2012-05-21 20:24:06 +0200

    Support creating Mac OS X bundles that embed all dependencies.

---
+++ share/macosx/Info.plist.cmake
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>CFBundleDevelopmentRegion</key>
+  <string>English</string>
+  <key>CFBundleDisplayName</key>
+  <string>${PROJECT_NAME}</string>
+  <key>CFBundleExecutable</key>
+  <string>${PROGNAME}</string>
+  <key>CFBundleIconFile</key>
+  <string>keepassx.icns</string>
+  <key>CFBundleIdentifier</key>
+  <string>org.keepassx.keepassx</string>
+  <key>CFBundleInfoDictionaryVersion</key>
+  <string>6.0</string>
+  <key>CFBundleName</key>
+  <string>${PROJECT_NAME}</string>
+  <key>CFBundlePackageType</key>
+  <string>APPL</string>
+  <key>CFBundleShortVersionString</key>
+  <string>${KEEPASSX_VERSION}</string>
+  <key>CFBundleSignature</key>
+  <string>KEPX</string>
+  <key>CFBundleVersion</key>
+  <string>${KEEPASSX_VERSION_NUM}</string>
+  <key>NSHumanReadableCopyright</key>
+    <string>Copyright 2010-2012 KeePassX Development Team</string>
+    <key>CFBundleDocumentTypes</key>
+    <array>
+      <dict>
+        <key>CFBundleTypeExtensions</key>
+        <array>
+          <string>kdbx</string>
+        </array>
+        <key>CFBundleTypeIconFile</key>
+        <string>keepassx.icns</string>
+        <key>CFBundleTypeName</key>
+        <string>KeePassX Password Database</string>
+        <key>CFBundleTypeOSTypes</key>
+        <array>
+          <string>kdbx</string>
+        </array>
+        <key>CFBundleTypeRole</key>
+        <string>Editor</string>
+      </dict>
+    </array>
+  </dict>
+</plist>
