4daac65cc528c067c18f9dd6c5d390cd99777681
Florian Geyer <blueice@fobos.de>
2012-12-27 10:50:48 +0100

    Show correct group name when opening an entry from seach results.

--- src/gui/DatabaseWidget.cpp
+++ src/gui/DatabaseWidget.cpp
@@ -430,14 +430,9 @@ void DatabaseWidget::switchToEntryEdit(Entry* entry)
 
 void DatabaseWidget::switchToEntryEdit(Entry* entry, bool create)
 {
-    Group* group = m_groupView->currentGroup();
-    if (!group) {
-        Q_ASSERT(m_entryView->inEntryListMode());
-        group = m_lastGroup;
-    }
-    Q_ASSERT(group);
+    Q_ASSERT(entry->group());
 
-    m_editEntryWidget->loadEntry(entry, create, false, group->name(), m_db);
+    m_editEntryWidget->loadEntry(entry, create, false, entry->group()->name(), m_db);
     setCurrentWidget(m_editEntryWidget);
 }
 
