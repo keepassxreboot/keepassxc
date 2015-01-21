6e7f826e19620565d6b6757dd6164af74a95fd4f
Pedro Alves <pgalves@gmail.com>
2015-01-21 17:01:59 +0000

    Added licence header.

--- src/gui/MessageWidget.cpp
+++ src/gui/MessageWidget.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2015 Felix Geyer <devel@pgalves.com>
+ *  Copyright (C) 2015 Pedro Alves <devel@pgalves.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
--- src/gui/MessageWidget.h
+++ src/gui/MessageWidget.h
@@ -1,4 +1,19 @@
-
+/*
+ *  Copyright (C) 2015 Pedro Alves <devel@pgalves.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 or (at your option)
+ *  version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef MESSAGEWIDGET_H
 #define MESSAGEWIDGET_H
