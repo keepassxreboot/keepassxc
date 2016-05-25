d4ed4f93255e51ed7a7986e6040f240ac6488670
Felix Geyer <debfx@fobos.de>
2016-05-25 16:55:06 +0200

    Remember auto-type window size.
    Resize columns once when the entry list is set.
    
    Based on https://github.com/keepassx/keepassx/pull/158
    
    Closes #478

--- src/autotype/AutoTypeSelectDialog.cpp
+++ src/autotype/AutoTypeSelectDialog.cpp
@@ -20,10 +20,12 @@
 #include <QApplication>
 #include <QDesktopWidget>
 #include <QDialogButtonBox>
+#include <QHeaderView>
 #include <QLabel>
 #include <QVBoxLayout>
 
 #include "autotype/AutoTypeSelectView.h"
+#include "core/Config.h"
 #include "core/FilePath.h"
 #include "gui/entry/EntryModel.h"
 
@@ -39,11 +41,14 @@ AutoTypeSelectDialog::AutoTypeSelectDialog(QWidget* parent)
     setWindowTitle(tr("Auto-Type - KeePassX"));
     setWindowIcon(filePath()->applicationIcon());
 
-    QSize size(400, 250);
+    QRect screenGeometry = QApplication::desktop()->availableGeometry(QCursor::pos());
+    QSize size = config()->get("GUI/AutoTypeSelectDialogSize", QSize(400, 250)).toSize();
+    size.setWidth(qMin(size.width(), screenGeometry.width()));
+    size.setHeight(qMin(size.height(), screenGeometry.height()));
     resize(size);
 
     // move dialog to the center of the screen
-    QPoint screenCenter = QApplication::desktop()->availableGeometry(QCursor::pos()).center();
+    QPoint screenCenter = screenGeometry.center();
     move(screenCenter.x() - (size.width() / 2), screenCenter.y() - (size.height() / 2));
 
     QVBoxLayout* layout = new QVBoxLayout(this);
@@ -65,6 +70,15 @@ void AutoTypeSelectDialog::setEntries(const QList<Entry*>& entries, const QHash<
 {
     m_sequences = sequences;
     m_view->setEntryList(entries);
+
+    m_view->header()->resizeSections(QHeaderView::ResizeToContents);
+}
+
+void AutoTypeSelectDialog::done(int r)
+{
+    config()->set("GUI/AutoTypeSelectDialogSize", size());
+
+    QDialog::done(r);
 }
 
 void AutoTypeSelectDialog::emitEntryActivated(const QModelIndex& index)
