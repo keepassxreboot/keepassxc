d0bb6cfaa555a7d1dfdf9e8db921fecb58eefbbb
thez3ro <io@thezero.org>
2017-05-04 22:52:10 +0200

    Added freedesktop DBus, fixed codestyle

--- src/core/ScreenLockListenerWin.cpp
+++ src/core/ScreenLockListenerWin.cpp
@@ -8,11 +8,11 @@
  * See https://msdn.microsoft.com/en-us/library/aa383841(v=vs.85).aspx
  * See https://blogs.msdn.microsoft.com/oldnewthing/20060104-50/?p=32783
  */
-ScreenLockListenerWin::ScreenLockListenerWin(QWidget *parent) :
-    ScreenLockListenerPrivate(parent),
-    QAbstractNativeEventFilter()
+ScreenLockListenerWin::ScreenLockListenerWin(QWidget *parent) 
+    : ScreenLockListenerPrivate(parent)
+    , QAbstractNativeEventFilter()
 {
-    Q_ASSERT(parent!=NULL);
+    Q_ASSERT(parent != NULL);
     // On windows, we need to register for platform specific messages and
     // install a message handler for them
     QCoreApplication::instance()->installNativeEventFilter(this);
@@ -26,7 +26,7 @@ ScreenLockListenerWin::ScreenLockListenerWin(QWidget *parent) :
     // This call requests a notification for session changes
     if (!WTSRegisterSessionNotification(
                 reinterpret_cast<HWND>(parent->winId()),
-                NOTIFY_FOR_THIS_SESSION)){
+                NOTIFY_FOR_THIS_SESSION)) {
     }
 }
 
@@ -35,31 +35,31 @@ ScreenLockListenerWin::~ScreenLockListenerWin()
     HWND h= reinterpret_cast<HWND>(static_cast<QWidget*>(parent())->winId());
     WTSUnRegisterSessionNotification(h);
 
-    if(m_powernotificationhandle){
+    if (m_powernotificationhandle) {
         UnregisterPowerSettingNotification(reinterpret_cast<HPOWERNOTIFY>(m_powernotificationhandle));
     }
 }
 
 bool ScreenLockListenerWin::nativeEventFilter(const QByteArray &eventType, void *message, long *)
 {
-    if(eventType == "windows_generic_MSG" || eventType == "windows_dispatcher_MSG"){
+    if (eventType == "windows_generic_MSG" || eventType == "windows_dispatcher_MSG") {
         MSG* m = static_cast<MSG *>(message);
-        if(m->message == WM_POWERBROADCAST){
+        if (m->message == WM_POWERBROADCAST) {
             const POWERBROADCAST_SETTING* setting = reinterpret_cast<const POWERBROADCAST_SETTING*>(m->lParam);
-            if (setting->PowerSetting == GUID_LIDSWITCH_STATE_CHANGE){
+            if (setting->PowerSetting == GUID_LIDSWITCH_STATE_CHANGE) {
                 const DWORD* state = reinterpret_cast<const DWORD*>(&setting->Data);
-                if (*state == 0){
+                if (*state == 0) {
                     Q_EMIT screenLocked();
                     return true;
                 }
             }
         }
-        if(m->message == WM_WTSSESSION_CHANGE){
-            if (m->wParam==WTS_CONSOLE_DISCONNECT){
+        if (m->message == WM_WTSSESSION_CHANGE) {
+            if (m->wParam == WTS_CONSOLE_DISCONNECT) {
                 Q_EMIT screenLocked();
                 return true;
             }
-            if (m->wParam==WTS_SESSION_LOCK){
+            if (m->wParam == WTS_SESSION_LOCK) {
                 Q_EMIT screenLocked();
                 return true;
             }
