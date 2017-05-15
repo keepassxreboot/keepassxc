0f18c9fcf98dfed4cc423b1ba28555191d6cde09
Marco <test@test.com>
2017-05-15 21:48:47 +0200

    fix comments

--- src/autotype/AutoType.cpp
+++ src/autotype/AutoType.cpp
@@ -691,7 +691,7 @@ bool AutoType::checkSyntax(const QString &string)
 
 bool AutoType::checkHighDelay(const QString &string)
 {
-    QRegExp highDelay(".*\\{Delay\\s[0-9]{5,}\\}.*"); //the 3 means 3 digitnumbers are too much
+    QRegExp highDelay(".*\\{Delay\\s[0-9]{5,}\\}.*"); //5 digit numbers(10 seconds) are too much
     highDelay.setCaseSensitivity(Qt::CaseInsensitive);
     highDelay.setPatternSyntax(QRegExp::RegExp);
     return highDelay.exactMatch(string);
@@ -699,7 +699,7 @@ bool AutoType::checkHighDelay(const QString &string)
 
 bool AutoType::checkHighRepetition(const QString &string)
 {
-    QRegExp highRepetition(".*\\s[0-9]{3,}.*");
+    QRegExp highRepetition(".*\\s[0-9]{3,}.*");//3 digit numbers are too much
     highRepetition.setPatternSyntax(QRegExp::RegExp);
     return highRepetition.exactMatch(string);
 }
