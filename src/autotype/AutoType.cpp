fc7e327e8848863c9d3813441cf88a723ab0d234
Marco <test@test.com>
2017-05-16 10:54:15 +0200

    fix for loops now have diffrent variables. dont know why it worked before

--- src/autotype/AutoType.cpp
+++ src/autotype/AutoType.cpp
@@ -571,8 +571,8 @@ QList<AutoTypeAction *> AutoType::createActionFromTemplate(const QString &tmpl, 
     //allows to insert usernames and passwords multiple times
     if (!list.isEmpty()) {
         for (int i = 1; i < num; i++) {
-            for (int i = 0; i < resolved.size(); i++) {
-                list.append(list.at(i)->clone());
+            for (int j = 0; j < resolved.size(); j++) {
+                list.append(list.at(j)->clone());
             }
         }
     }
