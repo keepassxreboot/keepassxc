bfee7346699e73916edf13abe09f5c5ceb03a661
Jonathan White <support@dmapps.us>
2017-05-27 21:33:30 -0400

    Fixed variable naming error that prevented compiling on Windows

--- src/core/ScreenLockListenerWin.cpp
+++ src/core/ScreenLockListenerWin.cpp
@@ -52,8 +52,8 @@ ScreenLockListenerWin::~ScreenLockListenerWin()
     HWND h= reinterpret_cast<HWND>(static_cast<QWidget*>(parent())->winId());
     WTSUnRegisterSessionNotification(h);
 
-    if (m_powernotificationhandle) {
-        UnregisterPowerSettingNotification(reinterpret_cast<HPOWERNOTIFY>(m_powernotificationhandle));
+    if (m_powerNotificationHandle) {
+        UnregisterPowerSettingNotification(reinterpret_cast<HPOWERNOTIFY>(m_powerNotificationHandle));
     }
 }
 
