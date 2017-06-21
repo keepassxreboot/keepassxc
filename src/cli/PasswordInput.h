400073c7cc7a313038da5ae7e70149294b6a72c3
Weslly <weslly.honorato@gmail.com>
2017-06-21 00:05:24 -0300

    Disable stdin echo when entering passwords on cli

---
+++ src/cli/PasswordInput.h
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (C) 2017 KeePassXC Team <team@keepassxc.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 or (at your option)
+ *  version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef KEEPASSXC_PASSWORDINPUT_H
+#define KEEPASSXC_PASSWORDINPUT_H
+
+#include <QtCore/qglobal.h>
+
+class PasswordInput
+{
+public:
+    PasswordInput();
+    static void setStdinEcho(bool enable);
+    static QString getPassword();
+};
+
+#endif // KEEPASSXC_PASSWORDINPUT_H
