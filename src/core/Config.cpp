ceeeee3d76a5736772150fa1adb00e30f3c6459b
Toni Spets <toni.spets@iki.fi>
2017-06-28 19:22:59 +0300

    Split auto-type title and URL matching into separate options

--- src/core/Config.cpp
+++ src/core/Config.cpp
@@ -118,6 +118,7 @@ void Config::init(const QString& fileName)
     m_defaults.insert("MinimizeOnCopy", false);
     m_defaults.insert("UseGroupIconOnEntryCreation", false);
     m_defaults.insert("AutoTypeEntryTitleMatch", true);
+    m_defaults.insert("AutoTypeEntryURLMatch", true);
     m_defaults.insert("UseGroupIconOnEntryCreation", true);
     m_defaults.insert("IgnoreGroupExpansion", false);
     m_defaults.insert("security/clearclipboard", true);
