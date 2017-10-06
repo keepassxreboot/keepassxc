b6387f7709a4750b82498bf52b12e3205dd110e4
frostasm <frost.asm@gmail.com>
2017-10-06 18:22:59 +0300

    AutoType: resolve placeholders in window associations list

--- src/gui/entry/AutoTypeAssociationsModel.cpp
+++ src/gui/entry/AutoTypeAssociationsModel.cpp
@@ -17,9 +17,12 @@
 
 #include "AutoTypeAssociationsModel.h"
 
+#include "core/Entry.h"
+
 AutoTypeAssociationsModel::AutoTypeAssociationsModel(QObject* parent)
     : QAbstractListModel(parent)
     , m_autoTypeAssociations(nullptr)
+    , m_entry(nullptr)
 {
 }
 
@@ -44,6 +47,11 @@ void AutoTypeAssociationsModel::setAutoTypeAssociations(AutoTypeAssociations* au
     }
 
     endResetModel();
+}
+
+void AutoTypeAssociationsModel::setEntry(const Entry *entry)
+{
+    m_entry = entry;
 }
 
 int AutoTypeAssociationsModel::rowCount(const QModelIndex& parent) const
@@ -86,7 +94,12 @@ QVariant AutoTypeAssociationsModel::data(const QModelIndex& index, int role) con
 
     if (role == Qt::DisplayRole) {
         if (index.column() == 0) {
-            return m_autoTypeAssociations->get(index.row()).window;
+            QString window = m_autoTypeAssociations->get(index.row()).window;
+            if (m_entry) {
+                window = m_entry->maskPasswordPlaceholders(window);
+                window = m_entry->resolveMultiplePlaceholders(window);
+            }
+            return window;
         }
         else {
             QString sequence = m_autoTypeAssociations->get(index.row()).sequence;
