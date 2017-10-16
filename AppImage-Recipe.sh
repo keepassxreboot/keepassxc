1cbbcc0d87b8c3d453388b986d6535e5aa5f8967
Janek Bevendorff <janek@jbev.net>
2017-10-16 17:01:37 +0200

    Allow starting AppImage with custom extended QT_PLUGIN_PATH, resolves #1077

--- AppImage-Recipe.sh
+++ AppImage-Recipe.sh
@@ -73,7 +73,7 @@ cat << EOF > ./usr/bin/keepassxc_env
 #!/usr/bin/env bash
 #export QT_QPA_PLATFORMTHEME=gtk2
 export LD_LIBRARY_PATH="..$(dirname ${QT_PLUGIN_PATH})/lib:\${LD_LIBRARY_PATH}"
-export QT_PLUGIN_PATH="..${QT_PLUGIN_PATH}"
+export QT_PLUGIN_PATH="..${QT_PLUGIN_PATH}:\${KPXC_QT_PLUGIN_PATH}"
 
 # unset XDG_DATA_DIRS to make tray icon work in Ubuntu Unity
 # see https://github.com/probonopd/AppImageKit/issues/351
