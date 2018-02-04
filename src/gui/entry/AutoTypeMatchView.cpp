aa54c7b6b372100a56eb1772be3f87b0e57a94fd
thez3ro <io@thezero.org>
2018-02-04 23:32:51 +0100

    fix MatchView activation with Enter/Return on macOS

--- src/gui/entry/AutoTypeMatchView.cpp
+++ src/gui/entry/AutoTypeMatchView.cpp
@@ -50,6 +50,10 @@ void AutoTypeMatchView::keyPressEvent(QKeyEvent* event)
 {
     if ((event->key() == Qt::Key_Enter || event->key() == Qt::Key_Return) && currentIndex().isValid()) {
         emitMatchActivated(currentIndex());
+#ifdef Q_OS_MAC
+        // Pressing return does not emit the QTreeView::activated signal on mac os
+        emit activated(currentIndex());
+#endif
     }
 
     QTreeView::keyPressEvent(event);
