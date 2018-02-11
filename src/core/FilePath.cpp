keepassxreboot/keepassxc

commit 6723f4215aafbae4c4fe69e975e21caaad66f636

centralize every password generator default option. add extended ASCII to XC_HTTP generator

src/core/FilePath.cpp
@@ -91,6 +91,11 @@ QString FilePath::pluginPath(const QString& name)
     return QString();
 }
 
+QString FilePath::wordlistPath(const QString& name)
+{
+    return m_instance->dataPath("wordlists/" + name);
+}
+
 QIcon FilePath::applicationIcon()
 {
     bool darkIcon = useDarkIcon();

src/core/PassphraseGenerator.cpp
@@ -24,9 +24,12 @@
 #include "crypto/Random.h"
 #include "core/FilePath.h"
 
+const QString PassphraseGenerator::DefaultSeparator = " ";
+const QString PassphraseGenerator::DefaultWordList = "eff_large.wordlist";
+
 PassphraseGenerator::PassphraseGenerator()
     : m_wordCount(0)
-    , m_separator(' ')
+    , m_separator(PassphraseGenerator::DefaultSeparator)
 {
     
 }
@@ -76,7 +79,7 @@ void PassphraseGenerator::setWordList(QString path)
 
 void PassphraseGenerator::setDefaultWordList()
 {
-    const QString path = filePath()->dataPath("wordlists/eff_large.wordlist");
+    const QString path = filePath()->wordlistPath(PassphraseGenerator::DefaultWordList);
     setWordList(path);
 }
 

src/core/PassphraseGenerator.h
@@ -37,6 +37,8 @@ public:
     QString generatePassphrase() const;
 
     static const int DefaultWordCount = 7;
+    static const QString DefaultSeparator;
+    static const QString DefaultWordList;
 
 private:
     int m_wordCount;

src/browser/BrowserSettings.cpp
@@ -211,7 +211,7 @@ void BrowserSettings::setVivaldiSupport(bool enabled) {
 
 bool BrowserSettings::passwordUseNumbers()
 {
-    return config()->get("generator/Numbers", true).toBool();
+    return config()->get("generator/Numbers", PasswordGenerator::DefaultNumbers).toBool();
 }
 
 void BrowserSettings::setPasswordUseNumbers(bool useNumbers)
@@ -221,7 +221,7 @@ void BrowserSettings::setPasswordUseNumbers(bool useNumbers)
 
 bool BrowserSettings::passwordUseLowercase()
 {
-    return config()->get("generator/LowerCase", true).toBool();
+    return config()->get("generator/LowerCase", PasswordGenerator::DefaultLower).toBool();
 }
 
 void BrowserSettings::setPasswordUseLowercase(bool useLowercase)
@@ -231,7 +231,7 @@ void BrowserSettings::setPasswordUseLowercase(bool useLowercase)
 
 bool BrowserSettings::passwordUseUppercase()
 {
-    return config()->get("generator/UpperCase", true).toBool();
+    return config()->get("generator/UpperCase", PasswordGenerator::DefaultUpper).toBool();
 }
 
 void BrowserSettings::setPasswordUseUppercase(bool useUppercase)
@@ -241,7 +241,7 @@ void BrowserSettings::setPasswordUseUppercase(bool useUppercase)
 
 bool BrowserSettings::passwordUseSpecial()
 {
-    return config()->get("generator/SpecialChars", false).toBool();
+    return config()->get("generator/SpecialChars", PasswordGenerator::DefaultSpecial).toBool();
 }
 
 void BrowserSettings::setPasswordUseSpecial(bool useSpecial)
@@ -251,7 +251,7 @@ void BrowserSettings::setPasswordUseSpecial(bool useSpecial)
 
 bool BrowserSettings::passwordUseEASCII()
 {
-    return config()->get("generator/EASCII", false).toBool();
+    return config()->get("generator/EASCII", PasswordGenerator::DefaultEASCII).toBool();
 }
 
 void BrowserSettings::setPasswordUseEASCII(bool useEASCII)
@@ -261,7 +261,7 @@ void BrowserSettings::setPasswordUseEASCII(bool useEASCII)
 
 int BrowserSettings::passPhraseWordCount()
 {
-    return config()->get("generator/WordCount", 6).toInt();
+    return config()->get("generator/WordCount", PassphraseGenerator::DefaultWordCount).toInt();
 }
 
 void BrowserSettings::setPassPhraseWordCount(int wordCount)
@@ -271,7 +271,7 @@ void BrowserSettings::setPassPhraseWordCount(int wordCount)
 
 QString BrowserSettings::passPhraseWordSeparator()
 {
-    return config()->get("generator/WordSeparator", " ").toString();
+    return config()->get("generator/WordSeparator", PassphraseGenerator::DefaultSeparator).toString();
 }
 
 void BrowserSettings::setPassPhraseWordSeparator(QString separator)
@@ -291,7 +291,7 @@ void BrowserSettings::setGeneratorType(int type)
 
 bool BrowserSettings::passwordEveryGroup()
 {
-    return config()->get("generator/EnsureEvery", true).toBool();
+    return config()->get("generator/EnsureEvery", PasswordGenerator::DefaultFromEveryGroup).toBool();
 }
 
 void BrowserSettings::setPasswordEveryGroup(bool everyGroup)
@@ -301,7 +301,7 @@ void BrowserSettings::setPasswordEveryGroup(bool everyGroup)
 
 bool BrowserSettings::passwordExcludeAlike()
 {
-    return config()->get("generator/ExcludeAlike", true).toBool();
+    return config()->get("generator/ExcludeAlike", PasswordGenerator::DefaultLookAlike).toBool();
 }
 
 void BrowserSettings::setPasswordExcludeAlike(bool excludeAlike)
@@ -311,7 +311,7 @@ void BrowserSettings::setPasswordExcludeAlike(bool excludeAlike)
 
 int BrowserSettings::passwordLength()
 {
-    return config()->get("generator/Length", 20).toInt();
+    return config()->get("generator/Length", PasswordGenerator::DefaultLength).toInt();
 }
 
 void BrowserSettings::setPasswordLength(int length)

src/core/FilePath.h
@@ -27,6 +27,7 @@ class FilePath
 public:
     QString dataPath(const QString& name);
     QString pluginPath(const QString& name);
+    QString wordlistPath(const QString& name);
     QIcon applicationIcon();
     QIcon trayIconLocked();
     QIcon trayIconUnlocked();

src/core/PasswordGenerator.h
@@ -42,7 +42,8 @@ public:
     enum GeneratorFlag
     {
         ExcludeLookAlike   = 0x1,
-        CharFromEveryGroup = 0x2
+        CharFromEveryGroup = 0x2,
+        DefaultFlags       = ExcludeLookAlike | CharFromEveryGroup
     };
     Q_DECLARE_FLAGS(GeneratorFlags, GeneratorFlag)
 
@@ -60,6 +61,13 @@ public:
     int getbits() const;
 
     static const int DefaultLength = 16;
+    static const bool DefaultLower = (DefaultCharset & LowerLetters) != 0;
+    static const bool DefaultUpper = (DefaultCharset & UpperLetters) != 0;
+    static const bool DefaultNumbers = (DefaultCharset & Numbers) != 0;
+    static const bool DefaultSpecial = (DefaultCharset & SpecialCharacters) != 0;
+    static const bool DefaultEASCII = (DefaultCharset & EASCII) != 0;
+    static const bool DefaultLookAlike = (DefaultFlags & ExcludeLookAlike) != 0;
+    static const bool DefaultFromEveryGroup = (DefaultFlags & CharFromEveryGroup) != 0;
 
 private:
     QVector<PasswordGroup> passwordGroups() const;

src/gui/PasswordGeneratorWidget.cpp
@@ -68,9 +68,9 @@ PasswordGeneratorWidget::PasswordGeneratorWidget(QWidget* parent)
     }
 
     // set default separator to Space
-    m_ui->editWordSeparator->setText(" ");
+    m_ui->editWordSeparator->setText(PassphraseGenerator::DefaultSeparator);
 
-    QDir path(filePath()->dataPath("wordlists/"));
+    QDir path(filePath()->wordlistPath(""));
     QStringList files = path.entryList(QDir::Files);
     m_ui->comboBoxWordList->addItems(files);
     if (files.size() > 1) {
@@ -93,19 +93,19 @@ PasswordGeneratorWidget::~PasswordGeneratorWidget()
 void PasswordGeneratorWidget::loadSettings()
 {
     // Password config
-    m_ui->checkBoxLower->setChecked(config()->get("generator/LowerCase", true).toBool());
-    m_ui->checkBoxUpper->setChecked(config()->get("generator/UpperCase", true).toBool());
-    m_ui->checkBoxNumbers->setChecked(config()->get("generator/Numbers", true).toBool());
-    m_ui->checkBoxSpecialChars->setChecked(config()->get("generator/SpecialChars", false).toBool());
-    m_ui->checkBoxExtASCII->setChecked(config()->get("generator/EASCII", false).toBool());
-    m_ui->checkBoxExcludeAlike->setChecked(config()->get("generator/ExcludeAlike", true).toBool());
-    m_ui->checkBoxEnsureEvery->setChecked(config()->get("generator/EnsureEvery", true).toBool());
+    m_ui->checkBoxLower->setChecked(config()->get("generator/LowerCase", PasswordGenerator::DefaultLower).toBool());
+    m_ui->checkBoxUpper->setChecked(config()->get("generator/UpperCase", PasswordGenerator::DefaultUpper).toBool());
+    m_ui->checkBoxNumbers->setChecked(config()->get("generator/Numbers", PasswordGenerator::DefaultNumbers).toBool());
+    m_ui->checkBoxSpecialChars->setChecked(config()->get("generator/SpecialChars", PasswordGenerator::DefaultSpecial).toBool());
+    m_ui->checkBoxExtASCII->setChecked(config()->get("generator/EASCII", PasswordGenerator::DefaultEASCII).toBool());
+    m_ui->checkBoxExcludeAlike->setChecked(config()->get("generator/ExcludeAlike", PasswordGenerator::DefaultLookAlike).toBool());
+    m_ui->checkBoxEnsureEvery->setChecked(config()->get("generator/EnsureEvery", PasswordGenerator::DefaultFromEveryGroup).toBool());
     m_ui->spinBoxLength->setValue(config()->get("generator/Length", PasswordGenerator::DefaultLength).toInt());
 
     // Diceware config
-    m_ui->spinBoxWordCount->setValue(config()->get("generator/WordCount", 6).toInt());
-    m_ui->editWordSeparator->setText(config()->get("generator/WordSeparator", " ").toString());
-    m_ui->comboBoxWordList->setCurrentText(config()->get("generator/WordList", "eff_large.wordlist").toString());
+    m_ui->spinBoxWordCount->setValue(config()->get("generator/WordCount", PassphraseGenerator::DefaultWordCount).toInt());
+    m_ui->editWordSeparator->setText(config()->get("generator/WordSeparator", PassphraseGenerator::DefaultSeparator).toString());
+    m_ui->comboBoxWordList->setCurrentText(config()->get("generator/WordList", PassphraseGenerator::DefaultWordList).toString());
 
     // Password or diceware?
     m_ui->tabWidget->setCurrentIndex(config()->get("generator/Type", 0).toInt());
@@ -394,7 +394,7 @@ void PasswordGeneratorWidget::updateGenerator()
 
         m_dicewareGenerator->setWordCount(m_ui->spinBoxWordCount->value());
         if (!m_ui->comboBoxWordList->currentText().isEmpty()) {
-            QString path = filePath()->dataPath("wordlists/" + m_ui->comboBoxWordList->currentText());
+            QString path = filePath()->wordlistPath(m_ui->comboBoxWordList->currentText());
             m_dicewareGenerator->setWordList(path);
         }
         m_dicewareGenerator->setWordSeparator(m_ui->editWordSeparator->text());

src/http/HttpPasswordGeneratorWidget.cpp
@@ -48,13 +48,14 @@ HttpPasswordGeneratorWidget::~HttpPasswordGeneratorWidget()
 
 void HttpPasswordGeneratorWidget::loadSettings()
 {
-    m_ui->checkBoxLower->setChecked(config()->get("Http/generator/LowerCase", true).toBool());
-    m_ui->checkBoxUpper->setChecked(config()->get("Http/generator/UpperCase", true).toBool());
-    m_ui->checkBoxNumbers->setChecked(config()->get("Http/generator/Numbers", true).toBool());
-    m_ui->checkBoxSpecialChars->setChecked(config()->get("Http/generator/SpecialChars", false).toBool());
+    m_ui->checkBoxLower->setChecked(config()->get("Http/generator/LowerCase", PasswordGenerator::DefaultLower).toBool());
+    m_ui->checkBoxUpper->setChecked(config()->get("Http/generator/UpperCase", PasswordGenerator::DefaultUpper).toBool());
+    m_ui->checkBoxNumbers->setChecked(config()->get("Http/generator/Numbers", PasswordGenerator::DefaultNumbers).toBool());
+    m_ui->checkBoxSpecialChars->setChecked(config()->get("Http/generator/SpecialChars", PasswordGenerator::DefaultSpecial).toBool());
+    m_ui->checkBoxSpecialChars->setChecked(config()->get("Http/generator/EASCII", PasswordGenerator::DefaultEASCII).toBool());
 
-    m_ui->checkBoxExcludeAlike->setChecked(config()->get("Http/generator/ExcludeAlike", true).toBool());
-    m_ui->checkBoxEnsureEvery->setChecked(config()->get("Http/generator/EnsureEvery", true).toBool());
+    m_ui->checkBoxExcludeAlike->setChecked(config()->get("Http/generator/ExcludeAlike", PasswordGenerator::DefaultLookAlike).toBool());
+    m_ui->checkBoxEnsureEvery->setChecked(config()->get("Http/generator/EnsureEvery", PasswordGenerator::DefaultFromEveryGroup).toBool());
 
     m_ui->spinBoxLength->setValue(config()->get("Http/generator/Length", PasswordGenerator::DefaultLength).toInt());
 }
@@ -65,6 +66,7 @@ void HttpPasswordGeneratorWidget::saveSettings()
     config()->set("Http/generator/UpperCase", m_ui->checkBoxUpper->isChecked());
     config()->set("Http/generator/Numbers", m_ui->checkBoxNumbers->isChecked());
     config()->set("Http/generator/SpecialChars", m_ui->checkBoxSpecialChars->isChecked());
+    config()->set("Http/generator/EASCII", m_ui->checkBoxExtASCII->isChecked());
 
     config()->set("Http/generator/ExcludeAlike", m_ui->checkBoxExcludeAlike->isChecked());
     config()->set("Http/generator/EnsureEvery", m_ui->checkBoxEnsureEvery->isChecked());
@@ -118,6 +120,10 @@ PasswordGenerator::CharClasses HttpPasswordGeneratorWidget::charClasses()
 
     if (m_ui->checkBoxSpecialChars->isChecked()) {
         classes |= PasswordGenerator::SpecialCharacters;
+    }
+
+    if (m_ui->checkBoxExtASCII->isChecked()) {
+        classes |= PasswordGenerator::EASCII;
     }
 
     return classes;

commit ab3775d4c5bda89db948b0efaafd264a2ce9466c

Fix regression introduced in 6723f42 Use of QString for static DefaultSeparator lead to crashes on Windows

src/core/FilePath.cpp
@@ -93,7 +93,7 @@ QString FilePath::pluginPath(const QString& name)
 
 QString FilePath::wordlistPath(const QString& name)
 {
-    return m_instance->dataPath("wordlists/" + name);
+    return dataPath("wordlists/" + name);
 }
 
 QIcon FilePath::applicationIcon()

src/core/PassphraseGenerator.cpp
@@ -17,32 +17,31 @@
 
 #include "PassphraseGenerator.h"
 
-#include <math.h>
+#include <cmath>
 #include <QFile>
 #include <QTextStream>
 
 #include "crypto/Random.h"
 #include "core/FilePath.h"
 
-const QString PassphraseGenerator::DefaultSeparator = " ";
-const QString PassphraseGenerator::DefaultWordList = "eff_large.wordlist";
+const char* PassphraseGenerator::DefaultSeparator = " ";
+const char* PassphraseGenerator::DefaultWordList = "eff_large.wordlist";
 
 PassphraseGenerator::PassphraseGenerator()
     : m_wordCount(0)
     , m_separator(PassphraseGenerator::DefaultSeparator)
 {
-    
 }
 
-double PassphraseGenerator::calculateEntropy(QString passphrase)
+double PassphraseGenerator::calculateEntropy(const QString& passphrase)
 {
     Q_UNUSED(passphrase);
 
-    if (m_wordlist.size() == 0) {
-        return 0;
+    if (m_wordlist.isEmpty()) {
+        return 0.0;
     }
 
-    return log(m_wordlist.size()) / log(2.0) * m_wordCount;
+    return std::log2(m_wordlist.size()) * m_wordCount;
 }
 
 void PassphraseGenerator::setWordCount(int wordCount)
@@ -56,7 +55,7 @@ void PassphraseGenerator::setWordCount(int wordCount)
 
 }
 
-void PassphraseGenerator::setWordList(QString path)
+void PassphraseGenerator::setWordList(const QString& path)
 {
     m_wordlist.clear();
 
@@ -83,7 +82,7 @@ void PassphraseGenerator::setDefaultWordList()
     setWordList(path);
 }
 
-void PassphraseGenerator::setWordSeparator(QString separator) {
+void PassphraseGenerator::setWordSeparator(const QString& separator) {
     m_separator = separator;
 }
 
@@ -97,8 +96,8 @@ QString PassphraseGenerator::generatePassphrase() const
     }
 
     QStringList words;
-    for (int i = 0; i < m_wordCount; i++) {
-        int wordIndex = randomGen()->randomUInt(m_wordlist.length());
+    for (int i = 0; i < m_wordCount; ++i) {
+        int wordIndex = randomGen()->randomUInt(static_cast<quint32>(m_wordlist.length()));
         words.append(m_wordlist.at(wordIndex));
     }
 
@@ -111,9 +110,5 @@ bool PassphraseGenerator::isValid() const
        return false;
     }
 
-    if (m_wordlist.size() < 1000) {
-        return false;
-    }
-
-    return true;
+    return m_wordlist.size() >= 1000;
 }

src/core/PassphraseGenerator.h
@@ -26,26 +26,25 @@ class PassphraseGenerator
 {
 public:
     PassphraseGenerator();
+    Q_DISABLE_COPY(PassphraseGenerator)
 
-    double calculateEntropy(QString passphrase);
+    double calculateEntropy(const QString& passphrase);
     void setWordCount(int wordCount);
-    void setWordList(QString path);
+    void setWordList(const QString& path);
     void setDefaultWordList();
-    void setWordSeparator(QString separator);
+    void setWordSeparator(const QString& separator);
     bool isValid() const;
 
     QString generatePassphrase() const;
 
-    static const int DefaultWordCount = 7;
-    static const QString DefaultSeparator;
-    static const QString DefaultWordList;
+    static constexpr int DefaultWordCount = 7;
+    static const char* DefaultSeparator;
+    static const char* DefaultWordList;
 
 private:
     int m_wordCount;
     QString m_separator;
     QVector<QString> m_wordlist;
-
-    Q_DISABLE_COPY(PassphraseGenerator)
 };
 
 #endif // KEEPASSX_PASSPHRASEGENERATOR_H