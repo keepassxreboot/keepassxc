c2dc5fb24697d2a4d00b14352b2352be6d5345e4
Jonathan White <support@dmapps.us>
2018-06-10 17:41:42 -0400

    Transition to env commands, remove fluff
    * Remove unnecessary script files
    * Remove cache (not worth it)
    * Use argon2 from pacman
    * Use WITH_XC_ALL flag

--- ci/appveyor/test.sh
+++ ci/appveyor/test.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/bash
 
-export PATH="/mingw64/bin:$PATH"
-export PATH="/mingw64/lib:$PATH"
-
 cd /c/projects/keepassxc/build
 
-qmake --version
 make test ARGS+="-E testgui --output-on-failure --verbose"
 make test ARGS+="-R testgui --output-on-failure --verbose"
