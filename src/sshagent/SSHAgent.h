c47882b887c38578243f9a7b11534dd465ae22c7
Jonathan White <support@dmapps.us>
2018-07-06 22:32:44 -0400

    Fixed regression

--- src/sshagent/SSHAgent.h
+++ src/sshagent/SSHAgent.h
@@ -36,7 +36,7 @@ public:
     const QString errorString() const;
     bool isAgentRunning() const;
     bool addIdentity(OpenSSHKey& key, quint32 lifetime = 0, bool confirm = false);
-    bool removeIdentity(OpenSSHKey& key) const;
+    bool removeIdentity(OpenSSHKey& key);
     void removeIdentityAtLock(const OpenSSHKey& key, const QUuid& uuid);
 
 signals:
