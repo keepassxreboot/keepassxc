a64d2ec7167e3412da49223fbf6958abc200d7a3
Jonathan White <support@dmapps.us>
2018-09-19 12:00:46 -0400

    Revert appveyor addition and DeployQt cmake

--- ci/appveyor/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-cd /c/projects/keepassxc/build
-
-make -j2
--- ci/appveyor/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-cd /c/projects/keepassxc/build
-
-make test ARGS+="-E testgui --output-on-failure --verbose"
-make test ARGS+="-R testgui --output-on-failure --verbose"
