3eae419d2929ded2a1331be419856afd545b36c6
Kyle Kneitinger <kyle@kneit.in>
2018-09-23 16:59:27 -0700

    Use length of existing password for the password generator
    Add optional length parameter to PasswordGeneratorWidget::reset()
    (default=0), which when >0 sets the legth SpinBox's value to the value
    passed in. The EditEntryWidget already called reset() when the password
    was known, so the current length was just added to this call (accounts
    for empty passwords)
    
    If entry edit is cancelled, return the password generator length to the
    application default or last committed length if available. This is done
    by calling reseting the password generator in EditEntryWidget::cancel()
    when the edit page is being closed
    
    Fixes #2180

--- src/gui/PasswordGeneratorWidget.cpp
+++ src/gui/PasswordGeneratorWidget.cpp
@@ -179,9 +179,15 @@ void PasswordGeneratorWidget::saveSettings()
     config()->set("generator/Type", m_ui->tabWidget->currentIndex());
 }
 
-void PasswordGeneratorWidget::reset()
+void PasswordGeneratorWidget::reset(int length)
 {
     m_ui->editNewPassword->setText("");
+    if (length > 0) {
+        m_ui->spinBoxLength->setValue(length);
+    } else {
+        m_ui->spinBoxLength->setValue(config()->get("generator/Length", PasswordGenerator::DefaultLength).toInt());
+    }
+
     setStandaloneMode(false);
     setPasswordVisible(config()->get("security/passwordscleartext").toBool());
     updateGenerator();
