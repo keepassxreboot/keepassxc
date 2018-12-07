6ae6b7cc80a7a95b4a5ced1557f9c471dea51a98
louib <louisbvarin@gmail.com>
2018-12-07 14:36:10 -0500

    Fix date in header file.

--- src/cli/Create.h
+++ src/cli/Create.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2017 KeePassXC Team <team@keepassxc.org>
+ *  Copyright (C) 2018 KeePassXC Team <team@keepassxc.org>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
