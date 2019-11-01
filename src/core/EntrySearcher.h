951365991c65f9753a20177ca99ec0dfc56943bd
Aetf <aetf@unlimitedcodeworks.xyz>
2019-11-01 16:23:26 -0400

    Expose EntrySearcher's SearchTerm for internal code usage

--- src/core/EntrySearcher.h
+++ src/core/EntrySearcher.h
@@ -28,18 +28,6 @@ class Entry;
 class EntrySearcher
 {
 public:
-    explicit EntrySearcher(bool caseSensitive = false);
-
-    QList<Entry*> search(const QString& searchString, const Group* baseGroup, bool forceSearch = false);
-    QList<Entry*> repeat(const Group* baseGroup, bool forceSearch = false);
-
-    QList<Entry*> searchEntries(const QString& searchString, const QList<Entry*>& entries);
-    QList<Entry*> repeatEntries(const QList<Entry*>& entries);
-
-    void setCaseSensitive(bool state);
-    bool isCaseSensitive();
-
-private:
     enum class Field
     {
         Undefined,
@@ -48,7 +36,7 @@ private:
         Password,
         Url,
         Notes,
-        AttributeKey,
+        AttributeKV,
         Attachment,
         AttributeValue
     };
@@ -56,17 +44,32 @@ private:
     struct SearchTerm
     {
         Field field;
+        // only used when field == Field::AttributeValue
         QString word;
         QRegularExpression regex;
         bool exclude;
     };
 
+    explicit EntrySearcher(bool caseSensitive = false);
+
+    QList<Entry*> search(const QList<SearchTerm>& searchTerms, const Group* baseGroup, bool forceSearch = false);
+    QList<Entry*> search(const QString& searchString, const Group* baseGroup, bool forceSearch = false);
+    QList<Entry*> repeat(const Group* baseGroup, bool forceSearch = false);
+
+    QList<Entry*> searchEntries(const QList<SearchTerm>& searchTerms, const QList<Entry*>& entries);
+    QList<Entry*> searchEntries(const QString& searchString, const QList<Entry*>& entries);
+    QList<Entry*> repeatEntries(const QList<Entry*>& entries);
+
+    void setCaseSensitive(bool state);
+    bool isCaseSensitive();
+
+private:
     bool searchEntryImpl(Entry* entry);
     void parseSearchTerms(const QString& searchString);
 
     bool m_caseSensitive;
     QRegularExpression m_termParser;
-    QList<QSharedPointer<SearchTerm>> m_searchTerms;
+    QList<SearchTerm> m_searchTerms;
 
     friend class TestEntrySearcher;
 };
