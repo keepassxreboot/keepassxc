f2d2daa782454f2168cdd0f19c0438524f9982ce
Wolfram Rösler <wolfram@roesler-ac.de>
2019-12-28 23:51:03 +0100

    Use Qt::AA_UseHighDpiPixmaps on all platforms
    ... not only on Linux, in order to prevent icons from being fuzzy.
    
    Fixes #475

--- src/main.cpp
+++ src/main.cpp
@@ -50,9 +50,7 @@ int main(int argc, char** argv)
 
 #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#ifdef Q_OS_LINUX
     QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-#endif
 #endif
 
 #if QT_VERSION >= QT_VERSION_CHECK(5, 7, 0)
