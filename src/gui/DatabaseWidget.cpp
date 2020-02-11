d0f136a64086eac6d51dfac26aec81cae880bc65
Ojas Anand <work@hailmail.net>
2020-02-11 04:01:40 -0500

    Return focus to focusWidget

--- src/gui/DatabaseWidget.cpp
+++ src/gui/DatabaseWidget.cpp
@@ -1657,6 +1657,8 @@ bool DatabaseWidget::save()
     m_blockAutoSave = true;
     ++m_saveAttempts;
 
+    auto focusWidget = qApp->focusWidget();
+
     // TODO: Make this async
     // Lock out interactions
     m_entryView->setDisabled(true);
@@ -1671,7 +1673,9 @@ bool DatabaseWidget::save()
     m_entryView->setDisabled(false);
     m_groupView->setDisabled(false);
 
-    m_entryView->setFocus();
+    if (focusWidget) {
+        focusWidget->setFocus();
+    }
 
     if (ok) {
         m_saveAttempts = 0;
