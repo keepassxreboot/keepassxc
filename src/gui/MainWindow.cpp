4c7dde258a78906b08f154329d4f049aee2dc722
philip <leishman@web.de>
2020-04-17 11:33:27 +0200

    move new item entry in context menu

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -175,11 +175,10 @@ MainWindow::MainWindow()
     m_entryContextMenu->addAction(m_ui->actionEntryEdit);
     m_entryContextMenu->addAction(m_ui->actionEntryClone);
     m_entryContextMenu->addAction(m_ui->actionEntryDelete);
+    m_entryContextMenu->addAction(m_ui->actionEntryNew);
     m_entryContextMenu->addSeparator();
     m_entryContextMenu->addAction(m_ui->actionEntryOpenUrl);
     m_entryContextMenu->addAction(m_ui->actionEntryDownloadIcon);
-    m_entryContextMenu->addSeparator();
-    m_entryContextMenu->addAction(m_ui->actionEntryNew);
 
     m_entryNewContextMenu = new QMenu(this);
     m_entryNewContextMenu->addAction(m_ui->actionEntryNew);
