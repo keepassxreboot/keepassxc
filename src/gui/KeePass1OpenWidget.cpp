fd802090a7d92cd4e0aca6da295678be5f83ba26
Jonathan White <support@dmapps.us>
2020-06-06 09:54:57 -0400

    Fix minor UX issues with DB Open Widget
    * Only clear password field when switching tabs or minimizing. This prevents the setting "Remember Key Files and Hardware Keys" from being useless with multiple databases.
    * Convert key file field to Line Edit, simplifies usage. Fix clear field button as well.
    * Removed need for clearForms to check if the database is being opened (was a solution to tab switching while unlocking, no longer a problem).

--- src/gui/KeePass1OpenWidget.cpp
+++ src/gui/KeePass1OpenWidget.cpp
@@ -37,14 +37,10 @@ void KeePass1OpenWidget::openDatabase()
     KeePass1Reader reader;
 
     QString password;
-    QString keyFileName;
+    QString keyFileName = m_ui->keyFileLineEdit->text();
 
     if (!m_ui->editPassword->text().isEmpty() || m_retryUnlockWithEmptyPassword) {
         password = m_ui->editPassword->text();
-    }
-
-    if (!m_ui->comboKeyFile->currentText().isEmpty() && m_ui->comboKeyFile->currentData() != -1) {
-        keyFileName = m_ui->comboKeyFile->currentText();
     }
 
     QFile file(m_filename);
