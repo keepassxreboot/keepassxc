59bd238ae19bd93b8bdf632a55c42593b974861c
tWido <wido.tomas@gmail.com>
2020-12-04 14:01:36 +0100

    Hide keyfile path by default
    Fixes #5576

--- src/gui/DatabaseOpenWidget.cpp
+++ src/gui/DatabaseOpenWidget.cpp
@@ -165,6 +165,7 @@ void DatabaseOpenWidget::clearForms()
     m_ui->editPassword->setText("");
     m_ui->editPassword->setShowPassword(false);
     m_ui->keyFileLineEdit->clear();
+    m_ui->keyFileLineEdit->setShowPassword(false);
     m_ui->checkTouchID->setChecked(false);
     m_ui->challengeResponseCombo->clear();
     m_db.reset();
@@ -378,6 +379,7 @@ void DatabaseOpenWidget::browseKeyFile()
 void DatabaseOpenWidget::clearKeyFileText()
 {
     m_ui->keyFileLineEdit->clear();
+    m_ui->keyFileLineEdit->setShowPassword(false);
 }
 
 void DatabaseOpenWidget::pollHardwareKey()
