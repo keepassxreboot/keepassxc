8f9220ad0456e2d5498c9225cb71b43303dd8faf
Nicolas Montmarquette <nicolas@montmarquette.org>
2021-02-15 15:42:43 -0500

    Save wip --- add Debian/Ubuntu dependencies installation script

---
+++ scripts/install_deps_debian10.sh
@@ -0,0 +1,26 @@
+apt-get install -y        \
+    asciidoctor           \
+    bzip2                 \
+    cmake                 \
+    g++                   \
+    gcc                   \
+    libargon2-dev         \
+    libgcrypt20-dev       \
+    libmicrohttpd-dev     \
+    libqrencode-dev       \
+    libqt5svg5-dev        \
+    libqt5x11extras5-dev  \
+    libquazip5-dev        \
+    libreadline-dev       \
+    libsodium-dev         \
+    libxi-dev             \
+    libxtst-dev           \
+    libykpers-1-dev       \
+    libyubikey-dev        \
+    make                  \
+    qtbase5-dev           \
+    qtbase5-dev-tools     \
+    qtbase5-private-dev   \
+    qttools5-dev          \
+    qttools5-dev-tools    \
+    zlib1g-dev
