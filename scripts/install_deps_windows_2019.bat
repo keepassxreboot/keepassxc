493cbafe1595759a59bfb930d1d0c905b82993f9
Nicolas Montmarquette <nicolas@montmarquette.org>
2021-02-15 16:35:36 -0500

    save wip: attempt at adding Windows 10 build

---
+++ scripts/install_deps_windows_2019.bat
@@ -0,0 +1,7 @@
+REM *************************************************************************
+REM *
+REM * Windows 10 (xxxx) dependencies installation script
+REM *
+REM *************************************************************************
+
+ECHO "Hello from Windows 10"
