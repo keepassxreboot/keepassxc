612ed2ba618e3f32e30cbddcceffdc7de6f770ef
Nicolas Montmarquette <nicolas@montmarquette.org>
2021-02-15 19:42:41 -0500

    Save wip: Add macos job

---
+++ scripts/install_deps_macos_10.15.sh
@@ -0,0 +1,7 @@
+#
+# https://github.com/keepassxreboot/keepassxc/wiki/Set-up-Build-Environment-on-macOS
+#
+brew install \
+    cmake \
+    qt5 libgcrypt argon2 qrencode libsodium asciidoctor \
+    libyubikey ykpers quazip
