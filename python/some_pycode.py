687a8334e9532cb46e22cd7670b7fff40fd04a37
Nicolas Montmarquette <nicolas.montmarquette@autodesk.com>
2021-02-24 17:25:06 -0500

    Reformat Python files from pre-commit

--- python/some_pycode.py
+++ python/some_pycode.py
@@ -3,25 +3,27 @@
 # importing the hashlib module
 import hashlib
 
+
 def hash_file(filename):
-   """"This function returns the SHA-1 hash
-   of the file passed into it"""
+    """ "This function returns the SHA-1 hash
+    of the file passed into it"""
 
-   # make a hash object
-   h = hashlib.sha1()
+    # make a hash object
+    h = hashlib.sha1()
 
-   # open file for reading in binary mode
-   with open(filename,'rb') as file:
+    # open file for reading in binary mode
+    with open(filename, "rb") as file:
 
-       # loop till the end of the file
-       chunk = 0
-       while chunk != b'':
-           # read only 1024 bytes at a time
-           chunk = file.read(1024)
-           h.update(chunk)
+        # loop till the end of the file
+        chunk = 0
+        while chunk != b"":
+            # read only 1024 bytes at a time
+            chunk = file.read(1024)
+            h.update(chunk)
 
-   # return the hex representation of digest
-   return h.hexdigest()
+    # return the hex representation of digest
+    return h.hexdigest()
+
 
 message = hash_file("track1.mp3")
 print(message)
