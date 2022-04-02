53d88dfe07a5242ee26c9dae3062340b85e8e952
Toni Spets <toni.spets@iki.fi>
2022-04-01 20:39:47 -0700

    SSH Agent: Fix Windows agent selection, radio buttons

--- src/core/Config.cpp
+++ src/core/Config.cpp
@@ -170,7 +170,7 @@ static const QHash<Config::ConfigKey, ConfigDirective> configStrings = {
     // SSHAgent
     {Config::SSHAgent_Enabled, {QS("SSHAgent/Enabled"), Roaming, false}},
     {Config::SSHAgent_UseOpenSSH, {QS("SSHAgent/UseOpenSSH"), Roaming, false}},
-    {Config::SSHAgent_UsePageant, {QS("SSHAgent/UsePageant"), Roaming, false} },
+    {Config::SSHAgent_UsePageant, {QS("SSHAgent/UsePageant"), Roaming, true} },
     {Config::SSHAgent_AuthSockOverride, {QS("SSHAgent/AuthSockOverride"), Local, {}}},
     {Config::SSHAgent_SecurityKeyProviderOverride, {QS("SSHAgent/SecurityKeyProviderOverride"), Local, {}}},
 
