25fc69dcd40ac9ae678caf63eaba0511d6985621
Jonathan White <support@dmapps.us>
2022-09-11 09:21:06 -0400

    Support {MODE=VIRTUAL} on macOS
    * Fix #8433

--- src/autotype/AutoTypeSelectDialog.cpp
+++ src/autotype/AutoTypeSelectDialog.cpp
@@ -326,7 +326,7 @@ void AutoTypeSelectDialog::buildActionMenu()
         submitAutoTypeMatch(match);
     });
 
-#ifdef Q_OS_WIN
+#if defined(Q_OS_WIN) || defined(Q_OS_MAC)
     auto typeVirtualAction = new QAction(icons()->icon("auto-type"), tr("Use Virtual Keyboard"));
     m_actionMenu->insertAction(copyUsernameAction, typeVirtualAction);
     typeVirtualAction->setShortcut(Qt::CTRL + Qt::Key_4);
@@ -342,7 +342,7 @@ void AutoTypeSelectDialog::buildActionMenu()
     typeUsernameAction->setShortcutVisibleInContextMenu(true);
     typePasswordAction->setShortcutVisibleInContextMenu(true);
     typeTotpAction->setShortcutVisibleInContextMenu(true);
-#ifdef Q_OS_WIN
+#if defined(Q_OS_WIN) || defined(Q_OS_MAC)
     typeVirtualAction->setShortcutVisibleInContextMenu(true);
 #endif
 #endif
