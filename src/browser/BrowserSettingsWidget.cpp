742a4f898090e238277928da270256f5a2d9409c
Jonathan White <support@dmapps.us>
2023-08-14 22:24:18 -0400

    Exclude Flatpak and Snap from proxy detection warning

--- src/browser/BrowserSettingsWidget.cpp
+++ src/browser/BrowserSettingsWidget.cpp
@@ -198,6 +198,7 @@ QString BrowserSettingsWidget::resolveCustomProxyLocation()
 
 void BrowserSettingsWidget::validateProxyLocation()
 {
+#if !defined(KEEPASSXC_DIST_SNAP) && !defined(KEEPASSXC_DIST_FLATPAK)
     // Reset the UI
     m_ui->messageWidget->setVisible(false);
     m_ui->customProxyLocation->setStyleSheet("");
@@ -228,6 +229,7 @@ void BrowserSettingsWidget::validateProxyLocation()
             }
         }
     }
+#endif
 }
 
 void BrowserSettingsWidget::saveSettings()
