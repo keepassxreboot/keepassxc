ce01f6d7cd97e7b167adbeedbd23b38885e097f1
f4lkensmaz3 <96619492+f4lkensmaz3@users.noreply.github.com>
2024-01-06 21:54:30 +0000

    Prevent duplicate characters in "Also choose from" field of password generator  (#9803)
    * Fixes #9797

--- src/core/PasswordGenerator.cpp
+++ src/core/PasswordGenerator.cpp
@@ -261,8 +261,10 @@ QVector<PasswordGroup> PasswordGenerator::passwordGroups() const
     if (!m_custom.isEmpty()) {
         PasswordGroup group;
 
-        for (auto ch : m_custom) {
-            group.append(ch);
+        for (const auto& ch : m_custom) {
+            if (!group.contains(ch)) {
+                group.append(ch);
+            }
         }
 
         passwordGroups.append(group);
