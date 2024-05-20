e93cfbc2e4763cbe7dedc09925f4752e61e2cf73
Sebastian Lipponer <mail@sebastianlipponer.de>
2024-05-20 09:54:34 +0200

    Move src/zxcvbn/ to src/thirdparty/zxcvbn

--- cmake/CLangFormat.cmake
+++ cmake/CLangFormat.cmake
@@ -16,7 +16,6 @@
 set(EXCLUDED_DIRS
         # third-party directories
         src/thirdparty
-        src/zxcvbn
         # objective-c directories
         src/quickunlock/touchid
         src/autotype/mac
