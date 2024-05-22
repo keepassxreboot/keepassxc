2a6916a018828395ad7f6dc3c8bd281b45c3f331
Jonathan White <support@dmapps.us>
2024-05-22 19:14:07 -0400

    Fix Alt modifier on Windows Auto-Type
    * Fixes #10791

--- src/gui/osutils/winutils/WinUtils.cpp
+++ src/gui/osutils/winutils/WinUtils.cpp
@@ -234,6 +234,8 @@ WORD WinUtils::qtToNativeKeyCode(Qt::Key key)
         return VK_SHIFT;    // 0x10
     case Qt::Key_Control:
         return VK_CONTROL;  // 0x11
+    case Qt::Key_Alt:
+        return VK_MENU;     // 0x12
     case Qt::Key_Pause:
         return VK_PAUSE;    // 0x13
     case Qt::Key_CapsLock:
