f948a415f3141fa3a911758fa048ee38a41b717f
Jonathan White <support@dmapps.us>
2024-09-22 21:23:54 -0400

    Improve related splitter UX
    * Prevent group pane from being hidden just by dragging. Introduce new View menu setting to hide the group pane.
    * Replace the preview panel "close" icon with a "collapse down" icon making the intention clearer.
    * Better organize the view menu

--- src/gui/DatabaseWidget.h
+++ src/gui/DatabaseWidget.h
@@ -288,6 +288,7 @@ private slots:
     // Database autoreload slots
     void reloadDatabaseFile();
     void restoreGroupEntryFocus(const QUuid& groupUuid, const QUuid& EntryUuid);
+    void onConfigChanged(Config::ConfigKey key);
 
 private:
     int addChildWidget(QWidget* w);
