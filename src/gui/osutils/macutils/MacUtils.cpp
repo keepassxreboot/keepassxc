777f9110105e4912f09716aef597679a7dc915bf
Vallode <18506096+vallode@users.noreply.github.com>
2024-10-16 17:10:39 +0200

    Fix MacOS login items showing ambigious name

--- src/gui/osutils/macutils/MacUtils.cpp
+++ src/gui/osutils/macutils/MacUtils.cpp
@@ -130,6 +130,8 @@ void MacUtils::setLaunchAtStartup(bool enable)
     if (enable) {
         QSettings agent(getLaunchAgentFilename(), QSettings::NativeFormat);
         agent.setValue("Label", qApp->property("KPXC_QUALIFIED_APPNAME").toString());
+        agent.setValue("AssociatedBundleIdentifiers", qApp->property("KPXC_QUALIFIED_APPNAME").toString());
+        agent.setValue("Program", QApplication::applicationFilePath());
         agent.setValue("ProgramArguments", QStringList() << QApplication::applicationFilePath());
         agent.setValue("RunAtLoad", true);
         agent.setValue("StandardErrorPath", "/dev/null");
