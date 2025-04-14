bf4b61063b1cf2842214474c76e3b4041048a935
xboxones1 <91512529+xboxones1@users.noreply.github.com>
2025-04-14 02:24:43 +0000

    Fix font size reset when changing theme

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -2004,6 +2004,7 @@ void MainWindow::initViewMenu()
             restartApp(tr("You must restart the application to apply this setting. Would you like to restart now?"));
         } else {
             kpxcApp->applyTheme();
+            kpxcApp->applyFontSize();
         }
     });
 
