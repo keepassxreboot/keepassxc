d61959466e304d6cebf4b78cfcf16041b2d37683
Darren Salt <devspam@moreofthesa.me.uk>
2025-05-26 02:07:12 +0100

    Bring the database open/unlock dialogue box into line with other on-top windows.

--- src/gui/DatabaseOpenDialog.cpp
+++ src/gui/DatabaseOpenDialog.cpp
@@ -35,12 +35,8 @@ DatabaseOpenDialog::DatabaseOpenDialog(QWidget* parent)
     , m_tabBar(new QTabBar(this))
 {
     setWindowTitle(tr("Unlock Database - KeePassXC"));
-    setWindowFlags(Qt::Dialog);
+    setWindowFlags(Qt::Dialog | Qt::WindowStaysOnTopHint);
     setWindowFlag(Qt::WindowContextHelpButtonHint, false);
-#ifdef Q_OS_LINUX
-    // Linux requires this to overcome some Desktop Environments (also no Quick Unlock)
-    setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint);
-#endif
     // block input to the main window/application while the dialog is open
     setWindowModality(Qt::ApplicationModal);
 #ifdef Q_OS_WIN
