48ad5044cbc883e886599a0823ca2583962b19e9
varjolintu <sami.vanttinen@protonmail.com>
2025-08-24 11:30:29 +0300

    Browser: Do not allow site automatically

--- src/browser/BrowserService.cpp
+++ src/browser/BrowserService.cpp
@@ -898,16 +898,6 @@ void BrowserService::addEntry(const EntryParameters& entryParameters,
 
     const QString host = QUrl(entryParameters.siteUrl).host();
     const QString submitHost = QUrl(entryParameters.formUrl).host();
-    BrowserEntryConfig config;
-    config.allow(host);
-
-    if (!submitHost.isEmpty()) {
-        config.allow(submitHost);
-    }
-    if (!entryParameters.realm.isEmpty()) {
-        config.setRealm(entryParameters.realm);
-    }
-    config.save(entry);
 
     if (downloadFavicon && m_currentDatabaseWidget) {
         m_currentDatabaseWidget->downloadFaviconInBackground(entry);
