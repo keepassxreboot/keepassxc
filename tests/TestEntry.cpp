0c99f6ecf1ab0332b333b653f2808fb35b79898a
Jonathan White <support@dmapps.us>
2025-09-27 15:07:24 -0400

    code format

--- tests/TestEntry.cpp
+++ tests/TestEntry.cpp
@@ -17,7 +17,6 @@
  */
 
 #include <QTest>
-#include <QDebug>
 
 #include "TestEntry.h"
 #include "core/Clock.h"
@@ -478,12 +477,12 @@ void TestEntry::testResolveUuidPlaceholder()
     entry->attributes()->set("SelfReference", "{REF:U@I:{UUID}}");
     QString attributeValue = entry->attributes()->value("SelfReference");
     QString resolvedSelfRef = entry->resolveMultiplePlaceholders(attributeValue);
-    
+
     // Test the manual reference to confirm it works as before
     QString manualReference = QString("{REF:U@I:%1}").arg(entry->uuidToHex());
     entry->attributes()->set("ManualReference", manualReference);
     QString resolvedManualRef = entry->resolveMultiplePlaceholders(entry->attributes()->value("ManualReference"));
-    
+
     // Test that both approaches work
     QCOMPARE(resolvedManualRef, entry->username());
     QCOMPARE(resolvedSelfRef, entry->username());
