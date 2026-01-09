fe2a999ad18bacd44c0b65fd54c3d4079a4066b6
Pranmya Joshi <pranmya04@gmail.com>
2026-01-09 13:09:18 +0530

    Refactor: use hasSelection() and remove commented code

--- src/gui/EditWidgetProperties.cpp
+++ src/gui/EditWidgetProperties.cpp
@@ -116,7 +116,6 @@ void EditWidgetProperties::update()
             m_customDataModel->appendRow(QList<QStandardItem*>()
                                          << new QStandardItem(key) << new QStandardItem(m_customData->value(key)));
         }
-        //m_ui->removeCustomDataButton->setEnabled(!m_customData->isEmpty());
-        m_ui->removeCustomDataButton->setEnabled(!m_ui->customDataTable->selectionModel()->selectedIndexes().isEmpty());
+        m_ui->removeCustomDataButton->setEnabled(m_ui->customDataTable->selectionModel()->hasSelection());
     }
 }
