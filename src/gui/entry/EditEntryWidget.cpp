ccc9e513312c10efed7d6edaac4d7c65b378550d
varjolintu <sami.vanttinen@ahmala.org>
2026-01-17 09:09:21 +0200

    Fix main URL validation when using placeholders

--- src/gui/entry/EditEntryWidget.cpp
+++ src/gui/entry/EditEntryWidget.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2023 KeePassXC Team <team@keepassxc.org>
+ *  Copyright (C) 2026 KeePassXC Team <team@keepassxc.org>
  *  Copyright (C) 2010 Felix Geyer <debfx@fobos.de>
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -1003,6 +1003,7 @@ void EditEntryWidget::setForms(Entry* entry, bool restore)
     m_autoTypeUi->windowSequenceEdit->setReadOnly(m_history);
     m_historyWidget->setEnabled(!m_history);
 
+    m_mainUi->urlEdit->setEntry(entry);
     m_mainUi->titleEdit->setText(entry->title());
     m_mainUi->usernameComboBox->lineEdit()->setText(entry->username());
     m_mainUi->urlEdit->setText(entry->url());
