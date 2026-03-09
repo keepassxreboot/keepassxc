808ced63950ccfd1296812eb471f6008dbef9d0a
Alexandre Petit <62973006+AlexpFr@users.noreply.github.com>
2026-03-09 13:30:13 +0100

    SSH Agent: Remove unused members m_entryCertificate and m_attachmentsCertificate

--- src/gui/entry/EditEntryWidget.h
+++ src/gui/entry/EditEntryWidget.h
@@ -188,8 +188,6 @@ private:
 #ifdef WITH_XC_SSHAGENT
     KeeAgentSettings m_sshAgentSettings;
     QString m_pendingPrivateKey;
-    QPointer<Entry> m_entryCertificate;
-    const QScopedPointer<EntryAttachments> m_attachmentsCertificate;
 #endif
     const QScopedPointer<Ui::EditEntryWidgetMain> m_mainUi;
     const QScopedPointer<Ui::EditEntryWidgetAdvanced> m_advancedUi;
