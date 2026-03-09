028cdc0b419eca66cc6f740fc8357258b1f68e1f
Janek Bevendorff <janek@keepassxc.org>
2026-03-09 23:02:26 +0100

    Set OPENSSL_MODULES to invalid location

--- src/main.cpp
+++ src/main.cpp
@@ -54,9 +54,10 @@ int main(int argc, char** argv)
     QT_REQUIRE_VERSION(argc, argv, QT_VERSION_STR)
 
 #ifdef Q_OS_WIN
-    // Set OPENSSL_CONF to an invalid location to prevent DLL injection via openssl.cnf.
+    // Set OPENSSL_CONF and OPENSSL_MODULES to an invalid location to prevent DLL injection via openssl.cnf.
     // vcpkg by default hard-codes this to its packages location, which may be user-writable.
     qputenv("OPENSSL_CONF", "::");
+    qputenv("OPENSSL_MODULES", "::");
 #endif
 
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
