a84fccc012e9adb1e487cd33e9076242760f1315
Matt Ezell <ezell.matt@gmail.com>
2026-03-26 10:34:36 -0500

    Fix indentation of setWindowFlags line

--- src/gui/UpdateCheckDialog.cpp
+++ src/gui/UpdateCheckDialog.cpp
@@ -29,7 +29,7 @@ UpdateCheckDialog::UpdateCheckDialog(QWidget* parent)
     , m_ui(new Ui::UpdateCheckDialog())
 {
     m_ui->setupUi(this);
-        setWindowFlags(Qt::Dialog | Qt::WindowTitleHint | Qt::WindowCloseButtonHint);
+    setWindowFlags(Qt::Dialog | Qt::WindowTitleHint | Qt::WindowCloseButtonHint);
     setAttribute(Qt::WA_DeleteOnClose);
 
     m_ui->iconLabel->setPixmap(icons()->applicationIcon().pixmap(48));
