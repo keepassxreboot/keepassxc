2a29e54d760b8096c17c82a477ef30872e31dc11
caesarw <11749340+caesarw@users.noreply.github.com>
2026-05-27 18:56:29 +0200

    Remove ifdef guards

--- src/core/EntryAttachments.cpp
+++ src/core/EntryAttachments.cpp
@@ -27,9 +27,7 @@
 #include <QProcessEnvironment>
 #include <QScopeGuard>
 #include <QSet>
-#ifdef KEEPASSXC_DIST_FLATPAK
 #include <QStandardPaths>
-#endif
 #include <QTemporaryFile>
 #include <QUrl>
 
