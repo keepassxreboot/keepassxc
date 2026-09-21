941cbef6342c0943f268a7f02b293451e25be783
Adrien Ollier <aollier@users.noreply.github.com>
2026-09-21 10:15:55 +0200

    Add copyright for cmake/FindBotan.cmake

--- cmake/FindBotan.cmake
+++ cmake/FindBotan.cmake
@@ -1,3 +1,23 @@
+# Copyright (C) 2026 Adrien Ollier
+#
+# This file is part of KeePassXC (https://github.com/keepassxreboot/keepassxc).
+#
+# This file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 1.2 of the License, or
+# (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this file; if not, contact the Free Software Foundation
+# at licensing@fsf.org or write to the Free Software Foundation, Inc.,
+# 31 Milk Street, # 960789, Boston, MA 02196 USA.
+
+
 #[=======================================================================[.rst:
 FindBotan
 ---------
