c09a6e334b249a23e93d10c53e630ac6f9986655
Adrien Ollier <aollier@users.noreply.github.com>
2026-09-23 00:39:17 +0200

    Correct license header from LGPL-1.2 to LGPL-2.1

--- cmake/FindBotan.cmake
+++ cmake/FindBotan.cmake
@@ -4,7 +4,7 @@
 #
 # This file is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation; either version 1.2 of the License, or
+# the Free Software Foundation; either version 2.1 of the License, or
 # (at your option) any later version.
 #
 # This file is distributed in the hope that it will be useful,
